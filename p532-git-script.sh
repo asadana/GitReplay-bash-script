22b0eedc61b57d773cc5a44b6150b88a91fcf59e
Ankit Sadana <asadana@indiana.edu>
2015-11-24 18:41:05 -0500

    Inital bash file

---
+++ p532-git-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# GitReplay with Bash
+# P532 - Sub-project
+
+# Made By: Ankit Sadana, Mrunal Lele, Pranav Pande, Rohith, Sairam Rakshith
+# Made On: 11/24/2015
+
+# Last Edited By: Ankit Sadana
+# Last Edited On: 11/24/2015
+
+# Source : git@github.com:asadana/GitReplay-bash-script.git
+
+# This script is meant to take an SSH to the source repo in git
+# Example of SSH: git@github.com:user/repository-name.git
+
+echo Hello People
